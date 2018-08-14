e535c4d35fa2ac745000dfe9ff6fff206ff3d461
Dirgh Buch <dirgh.buch@tabcorp.com.au>
2018-08-14 14:35:21 +1000

    Made lambda compatible

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ var express = require('express')
   , app = express()
   , bodyParser = require('body-parser')
   , port = process.env.PORT || 3000
+  , awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
 
 app.set('views', __dirname + '/views')
 app.engine('jade', require('jade').__express)
 app.set('view engine', 'jade')
 
 app.use(express.static(__dirname + '/public'))
-app.use(bodyParser.json())
+app.use(awsServerlessExpressMiddleware.eventContext({ deleteHeaders: false }), bodyParser.json());
+
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(require('./controllers'))
 
 app.listen(port, function() {
   console.log('Listening on port ' + port)
 })
+
+module.exports = app;
